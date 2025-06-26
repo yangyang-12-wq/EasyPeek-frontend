2b557797c23ef542f99eeadc84efe13a479fde41
刘霄 <964514012@qq.com>
2025-06-27 03:33:36 +0800

    主页面制作

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Login from '../user/Login';
 import Register from '../user/Register';
 import Dashboard from '../user/Dashboard';
+import HomePage from '../pages/HomePage';
+
 const ProtectedRoute = ({ children }) => {
   const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true'; // 修复：正确获取登录状态
   return isLoggedIn ? children : <Navigate to="/login" replace />;
@@ -26,6 +28,14 @@ const router = createBrowserRouter([
     element: (
       <ProtectedRoute>
         <Dashboard />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: '/HomePage',
+    element: (
+      <ProtectedRoute>
+        <HomePage />
       </ProtectedRoute>
     )
   }
